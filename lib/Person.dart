f772c2057b51868fb436c5aa162b8bff99daee4f
YoungGyu <ygkang@crscube.co.kr>
2021-09-07 15:12:42 +0900

    BMI 계산기

--- lib/Person.dart
+++
@@ -1,6 +0,0 @@
-class Person {
-  String name = "";
-  int age = 0;
-
-  Person(name, age);
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,5 @@
-import 'package:firstflutter/FirstPage.dart';
-import 'package:firstflutter/Page1.dart';
-import 'package:firstflutter/Page2.dart';
-import 'package:firstflutter/Page3.dart';
-import 'package:firstflutter/SecondPage.dart';
+import 'package:firstflutter/BmiResult.dart';
 import 'package:flutter/material.dart';
-
-final dummyItems = [
-  'https://picsum.photos/id/237/200/300',
-  'https://picsum.photos/seed/picsum/200/300',
-  'https://picsum.photos/200/300?grayscale'
-];
 
 void main() {
   runApp(MyApp());
@@ -19,61 +9,93 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-      routes: {
-        '/first': (context) => FirstPage(),
-        '/second': (context) => SecondPage()
-      },
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var _index = 0; // 페이지 인덱스
-  var _pages = [Page1(), Page2(), Page3()];
+  final _fromKey = GlobalKey<FormState>();
+  final _heightController = TextEditingController();
+  final _weightController = TextEditingController();
+
+  @override
+  void dispose() {
+    _heightController.dispose();
+    _weightController.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('복잡한 UI', style: TextStyle(color: Colors.black)),
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.add),
-            color: Colors.black,
-          )
-        ],
+        title: Text('비만도 계산기'),
       ),
-      body: _pages[_index],
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (index) {
-          setState(() {
-            _index = index;
-          });
-        },
-        currentIndex: _index,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
-          BottomNavigationBarItem(icon: Icon(Icons.assignment), label: '이용서비스'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle), label: '내 정보'),
-        ],
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _fromKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), hintText: '키'),
+                keyboardType: TextInputType.number,
+                controller: _heightController,
+                validator: (value) {
+                  if (value!.trim().isEmpty) {
+                    return '키를 입력하세요';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), hintText: '몸무게'),
+                keyboardType: TextInputType.number,
+                controller: _weightController,
+                validator: (value) {
+                  if (value!.trim().isEmpty) {
+                    return '몸무게를 입력하세요';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 16.0),
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_fromKey.currentState!.validate()) {
+                      // 검증 처리
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => BmiResult(
+                                  double.parse(_heightController.text.trim()),
+                                  double.parse(
+                                      _weightController.text.trim()))));
+                    }
+                  },
+                  child: Text('결과'),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
