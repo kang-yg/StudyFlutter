df01be138a65200f3a816a40dbd448adb3c60afa
YoungGyu <ygkang@crscube.co.kr>
2021-09-15 14:44:51 +0900

    CarousleSlider 테스트

--- lib/main.dart
+++ lib/main.dart
@@ -1,165 +1,126 @@
-import 'dart:io';
-
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
 
-void main() => runApp(MaterialApp(home: QRViewExample()));
+void main() => runApp(MaterialApp(home: CarouseSliderExample()));
 
-class QRViewExample extends StatefulWidget {
-  const QRViewExample({
+class CarouseSliderExample extends StatefulWidget {
+  const CarouseSliderExample({
     Key? key,
   }) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() => _QRViewExampleState();
+  State<StatefulWidget> createState() => _CarouseSliderExampleState();
 }
 
-class _QRViewExampleState extends State<QRViewExample> {
-  Barcode? result;
-  QRViewController? controller;
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-
-  // In order to get hot reload to work we need to pause the camera if the platform
-  // is android, or resume the camera if the platform is iOS.
-  @override
-  void reassemble() {
-    super.reassemble();
-    if (Platform.isAndroid) {
-      controller?.pauseCamera();
-    }
-    controller?.resumeCamera();
-  }
-
+class _CarouseSliderExampleState extends State<CarouseSliderExample> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Expanded(flex: 4, child: _buildQrView(context)),
-          Expanded(
-            flex: 1,
-            child: FittedBox(
-              fit: BoxFit.contain,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  if (result != null)
-                    Text(
-                        'Barcode Type: ${describeEnum(result!.format)}   Data: ${result?.code}')
-                  else
-                    Text('Scan a code'),
-/*                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.all(8),
-                        child: RaisedButton(
-                            onPressed: () async {
-                              await controller?.toggleFlash();
-                              setState(() {});
-                            },
-                            child: FutureBuilder(
-                              future: controller?.getFlashStatus(),
-                              builder: (context, snapshot) {
-                                return Text('Flash: ${snapshot.data}');
-                              },
-                            )),
-                      ),
-                      Container(
-                        margin: EdgeInsets.all(8),
-                        child: RaisedButton(
-                            onPressed: () async {
-                              await controller?.flipCamera();
-                              setState(() {});
-                            },
-                            child: FutureBuilder(
-                              future: controller?.getCameraInfo(),
-                              builder: (context, snapshot) {
-                                if (snapshot.data != null) {
-                                  return Text(
-                                      'Camera facing ${describeEnum(snapshot.data.toString())}');
-                                } else {
-                                  return Text('loading');
-                                }
-                              },
-                            )),
-                      )
-                    ],
-                  ),*/
-/*                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.all(8),
-                        child: RaisedButton(
-                          onPressed: () async {
-                            await controller?.pauseCamera();
-                          },
-                          child: Text('pause', style: TextStyle(fontSize: 20)),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.all(8),
-                        child: RaisedButton(
-                          onPressed: () async {
-                            await controller?.resumeCamera();
-                          },
-                          child: Text('resume', style: TextStyle(fontSize: 20)),
-                        ),
-                      )
-                    ],
-                  ),*/
-                ],
-              ),
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text(
+            'CarouseSliderExample',
+            textAlign: TextAlign.left,
+          ),
+          backgroundColor: Colors.greenAccent,
+        ),
+        body: CarouselSlider(
+            items: items,
+            options: CarouselOptions(
+              height: 400,
+              aspectRatio: 16/9,
+              viewportFraction: 0.8,
+              initialPage: 0,
+              enableInfiniteScroll: true,
+              reverse: false,
+              autoPlay: true,
+              autoPlayInterval: Duration(seconds: 3),
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enlargeCenterPage: true,
+              onPageChanged: callbackFunction,
+              scrollDirection: Axis.horizontal,
+            )
+        ));
+  }
+
+  Widget _getPage(var index) {
+    var result;
+
+    switch (index) {
+      case 1:
+        result = _firstPage();
+        break;
+      case 2:
+        result = _secondPage();
+        break;
+
+      default:
+        result = _elsePage();
+        break;
+    }
+
+    return result;
+  }
+
+  Widget _firstPage() {
+    return Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(color: Colors.amber),
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              height: 10,
             ),
-          )
-        ],
-      ),
+            ElevatedButton(
+                onPressed: () {
+                  print('AAA');
+                },
+                child: Text('Button')),
+            ListView(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              children: List.generate(
+                  30,
+                  (index) => ListTile(
+                        title: Text('$index'),
+                      )),
+            )
+          ],
+        ));
+  }
+
+  Widget _secondPage() {
+    return Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(color: Colors.white),
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  print('AAA');
+                },
+                child: Text('Button')),
+            ListView(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              children: List.generate(
+                  30,
+                  (index) => ListTile(
+                        title: Text('$index'),
+                      )),
+            )
+          ],
+        ));
+  }
+
+  Widget _elsePage() {
+    return Center(
+      child: Text('else page'),
     );
-  }
-
-  Widget _buildQrView(BuildContext context) {
-    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
-    var scanArea = (MediaQuery.of(context).size.width < 400 ||
-            MediaQuery.of(context).size.height < 400)
-        ? 150.0
-        : 300.0;
-    // To ensure the Scanner view is properly sizes after rotation
-    // we need to listen for Flutter SizeChanged notification and update controller
-    return QRView(
-      key: qrKey,
-      // You can choose between CameraFacing.front or CameraFacing.back. Defaults to CameraFacing.back
-      // cameraFacing: CameraFacing.front,
-      onQRViewCreated: _onQRViewCreated,
-      // Choose formats you want to scan. Defaults to all formats.
-      // formatsAllowed: [BarcodeFormat.qrcode],
-      overlay: QrScannerOverlayShape(
-        borderColor: Colors.red,
-        borderRadius: 10,
-        borderLength: 30,
-        borderWidth: 10,
-        cutOutSize: scanArea,
-      ),
-    );
-  }
-
-  void _onQRViewCreated(QRViewController controller) {
-    setState(() {
-      this.controller = controller;
-    });
-    controller.scannedDataStream.listen((scanData) {
-      setState(() {
-        result = scanData;
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    controller?.dispose();
-    super.dispose();
   }
 }
